9d4683dedf83a784297d0f60d73486f5c45affa2
reza <69678342+rezac78@users.noreply.github.com>
2021-07-19 06:27:58 -0700

    oky

---
+++ script.js
@@ -0,0 +1,39 @@
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+var $ = document;
+
+const keys = $.querySelectorAll('.key');
+const whiteKeys = $.querySelectorAll('.key.white');
+const blackKeys = $.querySelectorAll('.key.black');
+
+keys.forEach(key => {
+  key.addEventListener('click', () => {
+    playNote(key)
+  })
+})
+
+
+document.addEventListener("keydown", e => {
+  const key = e.key;
+  // console.log(key);
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+  const blackKeyIndex = BLACK_KEYS.indexOf(key)
+  if(whiteKeyIndex > -1) {
+    playNote(whiteKeys[whiteKeyIndex])
+  } 
+  if(blackKeyIndex > -1) {
+    playNote(blackKeys[blackKeyIndex])
+  }
+})
+
+
+
+function playNote(key) {
+  const noteAudio = $.getElementById(key.dataset.note);
+  noteAudio.currentTime = 0;
+  noteAudio.play();
+  key.classList.add('active');
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active')
+  })
+}
